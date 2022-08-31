f6afb1aa4188c20b35ffedaf592dd359638d7f50
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-08-31 23:34:02 +0300

    implement Git file status enums

--- src/git.rs
+++ src/git.rs
@@ -1,6 +1,17 @@
 use std::io::Error;
 use std::process::{Command, Output};
-use std::str;
+
+#[derive(Debug)]
+enum GitFileStatus {
+    Unmodified,
+    Modified,
+    TypeChanged,
+    Added,
+    Deleted,
+    Renamed,
+    Copied,
+    Updated,
+}
 
 pub fn exec_git(arg: Vec<&str>) -> Result<Output, Error> {
     Command::new("git").args(arg).output()
@@ -8,9 +19,11 @@ pub fn exec_git(arg: Vec<&str>) -> Result<Output, Error> {
 
 pub fn is_git_repo_root_dir() -> bool {
     exec_git(vec!["rev-parse", "--git-dir"])
-        .map(|output| match str::from_utf8(&output.stdout).unwrap().trim() {
-            ".git" => true,
-            _ => false,
-        })
+        .map(
+            |output| match std::str::from_utf8(&output.stdout).unwrap().trim() {
+                ".git" => true,
+                _ => false,
+            },
+        )
         .expect("Failed to determine if current directory is a git repo")
 }
