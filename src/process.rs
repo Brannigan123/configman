95cd8842ebc62906ed22407d9c410661b4d445ad
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-08-31 22:59:46 +0300

    purge the git2 dependency

--- src/process.rs
+++ src/process.rs
@@ -1,7 +1,5 @@
 use crate::config::SAMPLE_CONFIG_CONTENT;
 use crate::fs::get_working_dir;
-
-use git2::Repository;
 use std::fs;
 
 /// It checks if a configuration file exists in the current working directory, and if it doesn't, it
@@ -16,12 +14,4 @@ pub fn init_working_dir() {
         }
     }
     println!("Using config file: {:?}", &config_path);
-}
-
-pub fn ensure_is_git_working_dir() -> Repository {
-    Repository::init(get_working_dir()).expect("Failed to initialize git repo")
-}
-
-pub fn clone_from_remote(url: &str) -> Repository {
-    Repository::clone(url, get_working_dir()).expect("Failed to clone repo")
 }
