6bf30f79627c9f18477d05d746b041f215417f26
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-08-31 22:57:37 +0300

    implement check for whether wdir is a git repo

--- src/git.rs
+++ src/git.rs
@@ -1,3 +1,16 @@
+use std::io::Error;
+use std::process::{Command, Output};
+use std::str;
+
 pub fn exec_git(arg: Vec<&str>) -> Result<Output, Error> {
     Command::new("git").args(arg).output()
+}
+
+pub fn is_git_repo_root_dir() -> bool {
+    exec_git(vec!["rev-parse", "--git-dir"])
+        .map(|output| match str::from_utf8(&output.stdout).unwrap().trim() {
+            ".git" => true,
+            _ => false,
+        })
+        .expect("Failed to determine if current directory is a git repo")
 }
