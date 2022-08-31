f1a8ac60a4155f452c59009ec3c95cd7fac6a3d2
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-08-31 23:36:39 +0300

    some description on various git stuff so far

--- src/git.rs
+++ src/git.rs
@@ -13,10 +13,24 @@ enum GitFileStatus {
     Updated,
 }
 
+/// `exec_git` takes a vector of strings and returns a `Result` of `Output` or `Error`
+/// 
+/// Arguments:
+/// 
+/// * `arg`: Vec<&str> - A vector of arguments to pass to the git command.
+/// 
+/// Returns:
+/// 
+/// A Result<Output, Error>
 pub fn exec_git(arg: Vec<&str>) -> Result<Output, Error> {
     Command::new("git").args(arg).output()
 }
 
+/// If the output of `git rev-parse --git-dir` is `.git`, then we're in a git repo
+/// 
+/// Returns:
+/// 
+/// A boolean value.
 pub fn is_git_repo_root_dir() -> bool {
     exec_git(vec!["rev-parse", "--git-dir"])
         .map(
@@ -28,6 +42,11 @@ pub fn is_git_repo_root_dir() -> bool {
         .expect("Failed to determine if current directory is a git repo")
 }
 
+/// It adds a file to the git index
+/// 
+/// Arguments:
+/// 
+/// * `path`: The path to the file to be indexed.
 pub fn index_file(path: &str) {
     exec_git(vec!["add", &path, "-s"]).expect(format!("{}", &path).as_str());
 }
