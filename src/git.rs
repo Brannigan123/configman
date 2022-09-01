a52a808743b47e755cfc42c95417eb34a314da99
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-09-01 15:04:55 +0300

    added more variants for git file status

--- src/git.rs
+++ src/git.rs
@@ -2,7 +2,7 @@ use std::io::Error;
 use std::process::{Command, Output};
 
 #[derive(Debug)]
-enum GitFileStatus {
+pub enum GitFileStatus {
     Unmodified,
     Modified,
     TypeChanged,
@@ -11,25 +11,27 @@ enum GitFileStatus {
     Renamed,
     Copied,
     Updated,
+    Untracked,
+    Ignored,
 }
 
 /// `exec_git` takes a vector of strings and returns a `Result` of `Output` or `Error`
-/// 
+///
 /// Arguments:
-/// 
+///
 /// * `arg`: Vec<&str> - A vector of arguments to pass to the git command.
-/// 
+///
 /// Returns:
-/// 
+///
 /// A Result<Output, Error>
 pub fn exec_git(arg: Vec<&str>) -> Result<Output, Error> {
     Command::new("git").args(arg).output()
 }
 
 /// If the output of `git rev-parse --git-dir` is `.git`, then we're in a git repo
-/// 
+///
 /// Returns:
-/// 
+///
 /// A boolean value.
 pub fn is_git_repo_root_dir() -> bool {
     exec_git(vec!["rev-parse", "--git-dir"])
@@ -43,9 +45,9 @@ pub fn is_git_repo_root_dir() -> bool {
 }
 
 /// It adds a file to the git index
-/// 
+///
 /// Arguments:
-/// 
+///
 /// * `path`: The path to the file to be indexed.
 pub fn index_file(path: &str) {
     exec_git(vec!["add", &path, "-s"]).expect(format!("{}", &path).as_str());
