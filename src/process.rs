0ebff69a59c7dbd1aba331387aa0b14d6e9fd636
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-09-01 22:37:55 +0300

    find matching files & destination substituitions

--- src/process.rs
+++ src/process.rs
@@ -1,8 +1,11 @@
+use capturing_glob::Entry;
+
+use crate::config::Config;
+use crate::config::Mapping;
 use crate::config::SAMPLE_CONFIG_CONTENT;
-use crate::fs::get_working_dir;
+use crate::fs::{get_matching_files, get_working_dir};
 use crate::git::{add_file, init_git, is_git_repo_root_dir};
 use std::fs;
-
 
 /// It checks if the current working directory has a configuration file, if not, it generates one, then
 /// it checks if the current working directory is a git repository, if not, it initializes one, then it
@@ -21,4 +24,48 @@ pub fn init_working_dir() {
     }
     add_file(&config_path.display().to_string());
     println!("Using config file: {:?}", &config_path);
+}
+
+pub fn get_found_mappings(config: &Config) -> Vec<Mapping> {
+    let mut found_mappings = Vec::new();
+    for mapping in &config.mappings {
+        for matched in &get_matching_files(&mapping.source).expect("Failed match files") {
+            let source = matched.path().display().to_string();
+            let destination = substitute_group_values(mapping, matched);
+            found_mappings.push(Mapping {
+                source,
+                destination,
+            });
+        }
+    }
+    return found_mappings;
+}
+
+fn substitute_group_values(mapping: &Mapping, matched: &Entry) -> String {
+    let mut destination = mapping.destination.clone();
+    let mut group_index: usize = 1;
+    loop {
+        match substitute_group_value(&destination, &matched, group_index) {
+            Some((dest, grp_index)) => {
+                destination = dest;
+                group_index = grp_index;
+            }
+            None => break,
+        }
+    }
+    destination
+}
+
+fn substitute_group_value(
+    destination: &String,
+    matched: &Entry,
+    position: usize,
+) -> Option<(String, usize)> {
+    matched.group(position).map(|group| {
+        let old = format!("({})", position);
+        (
+            destination.replace(old.as_str(), group.to_str().unwrap()),
+            position + 1,
+        )
+    })
 }
