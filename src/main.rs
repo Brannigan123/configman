f8ca8e3b13058d4c8cafa83ea7c80312b2aecd2e
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-09-04 01:03:58 +0300

    update main function to start execution of states

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 mod config;
 mod fs;
+mod git;
+mod menu;
+mod process;
 
-use crate::config::load_config;
+use menu::State;
+use std::env;
 
+/// It loads the config file, then if there are no arguments, it runs the menu in a loop, otherwise it
+/// runs the menu once and then exits
 fn main() {
-    let config = load_config("example/setup.cmf");
-    match config {
-        Ok(c) => println!("{c}"),
-        Err(e) => println!("{e}"),
-    };
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() <= 1 || args[0] == "-i" {
+        let state = State::ActionSelection;
+        menu::run(state);
+    } else if args.len() == 2 {
+        let state = State::from(args[1].as_str());
+        menu::run_once(state);
+    } else {
+        println!("Failed to understand passed arguments");
+    }
 }
